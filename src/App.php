e2a22013db460b61da649cdfe783451b4b5232f6
靳陆陆 <805964774@qq.com>
2021-06-03 10:18:00 +0800

    修改错误拦截

--- src/App.php
+++ src/App.php
@@ -3,7 +3,6 @@
 namespace ChengYi;
 
 use ChengYi\constant\ErrorNums;
-use ChengYi\exception\ChengYiException;
 
 /**
  * 框架不支持场景配置，继承重写getConfigPath
@@ -17,7 +16,6 @@ class App extends \think\App
      * 获取应用配置目录
      * @access public
      * @return string
-     * @throws \ChengYi\exception\ChengYiException
      */
     public function getConfigPath(): string {
         $scene = $this->env->get('config_scene', 'prod');
@@ -25,6 +23,6 @@ class App extends \think\App
         if (is_dir($configPath)) {
             return $configPath;
         }
-        throw new ChengYiException('场景配置文件不存在,scene:' . $scene, ErrorNums::DIRECTORY_NOT_EXISTS);
+        die(json_encode(['errno' => ErrorNums::DIRECTORY_NOT_EXISTS,'errmsg' => '场景配置不存在，scene：' . $scene]));
     }
 }
